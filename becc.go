17ff4e40ba8e52fba246106425833da520c59da2
artilugio0 <artilugio0@proton.me>
2026-02-16 15:57:30 -0300

    add implementations using big.Int

---
+++ becc.go
@@ -0,0 +1,280 @@
+package becc
+
+import (
+	"fmt"
+	"math/big"
+)
+
+var (
+	bi0  *big.Int = big.NewInt(0)
+	bi1  *big.Int = big.NewInt(1)
+	bi2  *big.Int = big.NewInt(2)
+	bi3  *big.Int = big.NewInt(3)
+	bi4  *big.Int = big.NewInt(4)
+	bi27 *big.Int = big.NewInt(27)
+)
+
+func modInverse(n, m *big.Int) *big.Int {
+	a0, a1 := big.NewInt(0), big.NewInt(1)
+	r0, r1 := new(big.Int).Set(m), new(big.Int).Mod(n, m)
+
+	for r1.Cmp(bi1) == 1 {
+		q := new(big.Int).Div(r0, r1)
+
+		a1New := new(big.Int).Set(a1)
+		a1New.Mul(a1New, q).Sub(a0, a1New)
+
+		r1New := new(big.Int).Set(r1)
+		r1New.Mul(r1New, q).Sub(r0, r1New)
+
+		a0, a1 = a1, a1New
+		r0, r1 = r1, r1New
+	}
+
+	if r1.Cmp(bi1) == 0 {
+		return a1.Mod(a1, m)
+	}
+
+	return nil
+}
+
+type EllipticCurve struct {
+	a, b *FieldElement
+	m    *big.Int
+}
+
+var ErrInvalidParameters error = fmt.Errorf("invalid elliptic curve parameters")
+
+func NewEllipticCurve(a, b, m *big.Int) (EllipticCurve, error) {
+	fa := NewFieldElement(a, m)
+	fb := NewFieldElement(b, m)
+
+	disc := fa.Mul(fa).Mul(NewFieldElement(bi4, m)).
+		Add(fb.Mul(fb).Mul(fb).Mul(NewFieldElement(bi27, m)))
+
+	if disc.Eq(NewFieldElement(bi0, m)) {
+		return EllipticCurve{}, ErrInvalidParameters
+	}
+
+	return EllipticCurve{
+		a: fa,
+		b: fb,
+		m: new(big.Int).Set(m),
+	}, nil
+}
+
+func (ec EllipticCurve) Infinity() Point {
+	return Point{
+		inf: true,
+		ec:  ec,
+		x:   NewFieldElement(bi0, ec.m),
+		y:   NewFieldElement(bi0, ec.m),
+	}
+}
+
+func (ec EllipticCurve) NewPoint(x, y *big.Int) Point {
+	return Point{
+		x:  NewFieldElement(x, ec.m),
+		y:  NewFieldElement(y, ec.m),
+		ec: ec,
+	}
+}
+
+func (ec EllipticCurve) IsOnCurve(p Point) bool {
+	if p.IsInfinity() {
+		return true
+	}
+
+	lhs := p.y.Mul(p.y)
+
+	rhs := p.x.Mul(p.x).Mul(p.x).
+		Add(p.x.Mul(ec.a)).
+		Add(ec.b)
+
+	return lhs.Eq(rhs)
+}
+
+type Point struct {
+	ec EllipticCurve
+
+	x, y *FieldElement
+	inf  bool
+}
+
+func (p Point) String() string {
+	if p.inf {
+		return "∞"
+	}
+	return fmt.Sprintf("(%s, %s)", p.x, p.y)
+}
+
+func (p Point) IsInfinity() bool {
+	return p.inf
+}
+
+func (p Point) Eq(q Point) bool {
+	return p.x.Eq(q.x) && p.y.Eq(q.y)
+}
+
+func (p Point) Neg() Point {
+	if p.inf {
+		return Point{
+			ec:  p.ec,
+			inf: true,
+			x:   &FieldElement{n: big.NewInt(0), m: p.x.m},
+			y:   &FieldElement{n: big.NewInt(0), m: p.y.m},
+		}
+	}
+
+	return Point{
+		ec: p.ec,
+		x:  &FieldElement{n: new(big.Int).Set(p.x.n), m: p.x.m},
+		y:  p.y.Neg(),
+	}
+}
+
+func (p Point) Add(q Point) Point {
+	if p.IsInfinity() {
+		return q
+	}
+
+	if q.IsInfinity() {
+		return p
+	}
+
+	if p.x.Eq(q.x) && !p.y.Eq(q.y) {
+		return p.ec.Infinity()
+	}
+
+	var slope *FieldElement
+	if p.x.Eq(q.x) && p.y.Eq(q.y) {
+		if p.y.IsZero() {
+			return p.ec.Infinity()
+		}
+
+		slope = p.x.Mul(p.x).MulInt(3).
+			Add(p.ec.a).
+			Mul(p.y.MulInt(2).ModInverse())
+
+	} else {
+		slope = q.y.Sub(p.y).Mul(q.x.Sub(p.x).ModInverse())
+	}
+
+	x := slope.Mul(slope).Sub(p.x).Sub(q.x)
+	y := slope.Mul(p.x.Sub(x)).Sub(p.y)
+
+	return Point{
+		x:  x,
+		y:  y,
+		ec: p.ec,
+	}
+}
+
+func (p Point) ScalarMul(k int64) Point {
+	if k < 0 {
+		p = p.Neg()
+		k = -k
+	}
+
+	result := p.ec.Infinity()
+	addend := p
+	for k > 0 {
+		if k&1 == 1 {
+			result = result.Add(addend)
+		}
+
+		k = k >> 1
+		addend = addend.Add(addend)
+	}
+
+	return result
+}
+
+type FieldElement struct {
+	m *big.Int
+	n *big.Int
+}
+
+func (fe *FieldElement) String() string {
+	return fmt.Sprintf("%s (mod %s)", fe.n, fe.m)
+}
+
+func NewFieldElement(n, m *big.Int) *FieldElement {
+	return &FieldElement{
+		m: new(big.Int).Set(m),
+		n: new(big.Int).Mod(n, m),
+	}
+}
+
+func NewFieldElementInt(n, m int64) *FieldElement {
+	mod := big.NewInt(m)
+	num := big.NewInt(n)
+	return &FieldElement{
+		m: mod,
+		n: num.Mod(num, mod),
+	}
+}
+
+func (fe *FieldElement) Add(n *FieldElement) *FieldElement {
+	result := new(big.Int).Add(fe.n, n.n)
+	return &FieldElement{
+		m: fe.m,
+		n: result.Mod(result, fe.m),
+	}
+}
+
+func (fe *FieldElement) Sub(n *FieldElement) *FieldElement {
+	result := new(big.Int).Sub(fe.n, n.n)
+	return &FieldElement{
+		m: fe.m,
+		n: result.Mod(result, fe.m),
+	}
+}
+
+func (fe *FieldElement) Mul(n *FieldElement) *FieldElement {
+	result := new(big.Int).Mul(fe.n, n.n)
+	return &FieldElement{
+		m: fe.m,
+		n: result.Mod(result, fe.m),
+	}
+}
+
+func (fe *FieldElement) MulInt(n int64) *FieldElement {
+	result := big.NewInt(n)
+	result.Mul(fe.n, result)
+
+	return &FieldElement{
+		m: fe.m,
+		n: result.Mod(result, fe.m),
+	}
+}
+
+func (fe *FieldElement) Neg() *FieldElement {
+	neg := new(big.Int).Sub(fe.m, fe.n)
+	neg.Mod(neg, fe.m)
+
+	return &FieldElement{
+		m: fe.m,
+		n: neg,
+	}
+}
+
+func (fe *FieldElement) ModInverse() *FieldElement {
+	inv := modInverse(fe.n, fe.m)
+	if inv == nil {
+		return nil
+	}
+
+	return &FieldElement{
+		m: fe.m,
+		n: inv,
+	}
+}
+
+func (fe *FieldElement) Eq(n *FieldElement) bool {
+	return fe.n.Cmp(n.n) == 0
+}
+
+func (fe *FieldElement) IsZero() bool {
+	return fe.n.Cmp(bi0) == 0
+}
---
+++ becc_test.go
@@ -0,0 +1,229 @@
+package becc
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+)
+
+func TestModInverse(t *testing.T) {
+	tt := []struct {
+		n        *big.Int
+		m        *big.Int
+		expected *big.Int
+	}{
+		{n: big.NewInt(1), m: big.NewInt(11), expected: big.NewInt(1)},
+		{n: big.NewInt(2), m: big.NewInt(11), expected: big.NewInt(6)},
+		{n: big.NewInt(3), m: big.NewInt(11), expected: big.NewInt(4)},
+		{n: big.NewInt(3), m: big.NewInt(11), expected: big.NewInt(4)},
+		{n: big.NewInt(3), m: big.NewInt(7), expected: big.NewInt(5)},
+		{n: big.NewInt(5), m: big.NewInt(12), expected: big.NewInt(5)},
+		{n: big.NewInt(7), m: big.NewInt(26), expected: big.NewInt(15)},
+		{n: big.NewInt(1), m: big.NewInt(10), expected: big.NewInt(1)},
+		{n: big.NewInt(2), m: big.NewInt(9), expected: big.NewInt(5)},
+		{n: big.NewInt(4), m: big.NewInt(9), expected: big.NewInt(7)},
+		{n: big.NewInt(8), m: big.NewInt(21), expected: big.NewInt(8)},
+		{n: big.NewInt(17), m: big.NewInt(61), expected: big.NewInt(18)},
+		{n: big.NewInt(42), m: big.NewInt(101), expected: big.NewInt(89)},
+		{n: big.NewInt(9), m: big.NewInt(100), expected: big.NewInt(89)},
+		{n: big.NewInt(89), m: big.NewInt(100), expected: big.NewInt(9)},
+		{n: big.NewInt(3), m: big.NewInt(17), expected: big.NewInt(6)},
+		{n: big.NewInt(10), m: big.NewInt(17), expected: big.NewInt(12)},
+		{n: big.NewInt(1), m: big.NewInt(17), expected: big.NewInt(1)},
+		{n: big.NewInt(15), m: big.NewInt(13), expected: big.NewInt(7)},
+		{n: big.NewInt(-11), m: big.NewInt(13), expected: big.NewInt(7)},
+		{n: big.NewInt(0), m: big.NewInt(17), expected: nil},
+		{n: big.NewInt(2), m: big.NewInt(4), expected: nil},
+		{n: big.NewInt(6), m: big.NewInt(15), expected: nil},
+		{n: big.NewInt(7), m: big.NewInt(21), expected: nil},
+	}
+
+	one := big.NewInt(1)
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("%d inverse mod %d", tc.n, tc.m), func(t *testing.T) {
+			got := modInverse(tc.n, tc.m)
+			if got == nil {
+				if tc.expected == nil {
+					return
+				}
+				t.Fatalf("got nil, expected: %v", tc.expected)
+			}
+
+			if got.Cmp(tc.expected) != 0 {
+				t.Errorf("got %d, expected %d", got, tc.expected)
+			}
+
+			prod := big.NewInt(0).Mul(tc.n, got)
+			prod.Mod(prod, tc.m)
+			if prod.Cmp(one) != 0 {
+				t.Errorf("%d * %d = %d != 1", tc.n, got, prod)
+			}
+		})
+	}
+}
+
+func TestEllipticCurveNewPoint(t *testing.T) {
+	ec, _ := NewEllipticCurve(big.NewInt(2), big.NewInt(2), big.NewInt(17))
+	tt := []struct {
+		x, y *big.Int
+		p    Point
+	}{
+		{x: big.NewInt(10), y: big.NewInt(5), p: ec.NewPoint(big.NewInt(10), big.NewInt(5))},
+		{x: big.NewInt(10), y: big.NewInt(19), p: ec.NewPoint(big.NewInt(10), big.NewInt(2))},
+		{x: big.NewInt(18), y: big.NewInt(15), p: ec.NewPoint(big.NewInt(1), big.NewInt(15))},
+	}
+
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("(%d, %d) (mod 17)", tc.x, tc.y), func(t *testing.T) {
+			p := ec.NewPoint(tc.x, tc.y)
+
+			if !p.Eq(tc.p) {
+				t.Errorf("got %+v, expected %+v", p, tc.p)
+			}
+		})
+	}
+}
+
+func TestInfinity(t *testing.T) {
+	ec, _ := NewEllipticCurve(big.NewInt(2), big.NewInt(2), big.NewInt(17))
+	if !ec.Infinity().IsInfinity() {
+		t.Errorf("expected IsInfinity to be true")
+	}
+}
+
+func TestPointNeg(t *testing.T) {
+	ec, _ := NewEllipticCurve(big.NewInt(2), big.NewInt(2), big.NewInt(17))
+
+	tt := []struct {
+		p   Point
+		neg Point
+	}{
+		{
+			p:   ec.NewPoint(big.NewInt(10), big.NewInt(5)),
+			neg: ec.NewPoint(big.NewInt(10), big.NewInt(12)),
+		},
+		{
+			p:   ec.NewPoint(big.NewInt(1), big.NewInt(16)),
+			neg: ec.NewPoint(big.NewInt(1), big.NewInt(1)),
+		},
+		{
+			p:   ec.NewPoint(big.NewInt(3), big.NewInt(1)),
+			neg: ec.NewPoint(big.NewInt(3), big.NewInt(16)),
+		},
+		{
+			p:   ec.Infinity(),
+			neg: ec.Infinity(),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("%+v", tc.p), func(t *testing.T) {
+			got := tc.p.Neg()
+
+			if !got.Eq(tc.neg) {
+				t.Errorf("got %+v, expected %+v", got, tc.neg)
+			}
+		})
+	}
+}
+
+func TestEllipticCurveIsOnCurve(t *testing.T) {
+	ec1, _ := NewEllipticCurve(big.NewInt(2), big.NewInt(2), big.NewInt(17))
+
+	tt := []struct {
+		ec       EllipticCurve
+		p        Point
+		expected bool
+	}{
+		{ec: ec1, p: ec1.NewPoint(big.NewInt(0), big.NewInt(6)), expected: true},
+		{ec: ec1, p: ec1.NewPoint(big.NewInt(3), big.NewInt(1)), expected: true},
+		{ec: ec1, p: ec1.NewPoint(big.NewInt(5), big.NewInt(1)), expected: true},
+		{ec: ec1, p: ec1.NewPoint(big.NewInt(10), big.NewInt(6)), expected: true},
+		{ec: ec1, p: ec1.NewPoint(big.NewInt(1), big.NewInt(2)), expected: false},
+		{ec: ec1, p: ec1.Infinity(), expected: true},
+	}
+
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("%+v", tc.p), func(t *testing.T) {
+			got := tc.ec.IsOnCurve(tc.p)
+
+			if got != tc.expected {
+				t.Errorf("got %+v, expected %+v", got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestPointAdd(t *testing.T) {
+	ec, _ := NewEllipticCurve(big.NewInt(1), big.NewInt(6), big.NewInt(11))
+	point24 := ec.NewPoint(big.NewInt(2), big.NewInt(4))
+	ec2_2_17, _ := NewEllipticCurve(big.NewInt(2), big.NewInt(2), big.NewInt(17))
+
+	tt := []struct {
+		p1       Point
+		p2       Point
+		expected Point
+	}{
+		{p1: ec.Infinity(), p2: ec.Infinity(), expected: ec.Infinity()},
+		{p1: ec.Infinity(), p2: point24, expected: point24},
+		{p1: point24, p2: ec.Infinity(), expected: point24},
+		{p1: point24, p2: ec.NewPoint(big.NewInt(3), big.NewInt(5)), expected: ec.NewPoint(big.NewInt(7), big.NewInt(2))},
+		{p1: point24, p2: ec.NewPoint(big.NewInt(2), big.NewInt(7)), expected: ec.Infinity()},
+
+		{p1: point24, p2: ec.NewPoint(big.NewInt(3), big.NewInt(6)), expected: ec.NewPoint(big.NewInt(10), big.NewInt(2))},
+		{p1: point24, p2: ec.NewPoint(big.NewInt(2), big.NewInt(4)), expected: ec.NewPoint(big.NewInt(5), big.NewInt(9))},
+		{p1: ec2_2_17.NewPoint(big.NewInt(0), big.NewInt(6)), p2: ec2_2_17.NewPoint(big.NewInt(3), big.NewInt(1)), expected: ec2_2_17.NewPoint(big.NewInt(13), big.NewInt(10))},
+		{p1: ec2_2_17.NewPoint(big.NewInt(0), big.NewInt(6)), p2: ec2_2_17.NewPoint(big.NewInt(0), big.NewInt(6)), expected: ec2_2_17.NewPoint(big.NewInt(9), big.NewInt(1))},
+	}
+
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("%+v + %v", tc.p1, tc.p2), func(t *testing.T) {
+			got := tc.p1.Add(tc.p2)
+
+			if !got.Eq(tc.expected) {
+				t.Errorf("got %+v, expected %+v", got, tc.expected)
+			}
+
+			if !tc.p1.ec.IsOnCurve(got) {
+				t.Errorf("the result (%+v) is not on curve", got)
+			}
+		})
+	}
+}
+
+func TestPointScalarMul(t *testing.T) {
+	ec, _ := NewEllipticCurve(big.NewInt(1), big.NewInt(6), big.NewInt(11))
+	ec2_2_17, _ := NewEllipticCurve(big.NewInt(2), big.NewInt(2), big.NewInt(17))
+
+	tt := []struct {
+		p        Point
+		k        int64
+		expected Point
+	}{
+		{p: ec.Infinity(), k: 100, expected: ec.Infinity()},
+		{p: ec.Infinity(), k: 0, expected: ec.Infinity()},
+		{p: ec.NewPoint(big.NewInt(2), big.NewInt(4)), k: 0, expected: ec.Infinity()},
+		{p: ec.NewPoint(big.NewInt(2), big.NewInt(4)), k: 2, expected: ec.NewPoint(big.NewInt(5), big.NewInt(9))},
+		{p: ec.NewPoint(big.NewInt(2), big.NewInt(4)), k: 3, expected: ec.NewPoint(big.NewInt(8), big.NewInt(8))},
+		{p: ec.NewPoint(big.NewInt(2), big.NewInt(4)), k: 4, expected: ec.NewPoint(big.NewInt(10), big.NewInt(9))},
+		{p: ec.NewPoint(big.NewInt(2), big.NewInt(4)), k: -1, expected: ec.NewPoint(big.NewInt(2), big.NewInt(7))},
+		{p: ec2_2_17.NewPoint(big.NewInt(0), big.NewInt(6)), k: 2, expected: ec2_2_17.NewPoint(big.NewInt(9), big.NewInt(1))},
+		{p: ec2_2_17.NewPoint(big.NewInt(0), big.NewInt(6)), k: 3, expected: ec2_2_17.NewPoint(big.NewInt(6), big.NewInt(3))},
+		{p: ec2_2_17.NewPoint(big.NewInt(0), big.NewInt(6)), k: 4, expected: ec2_2_17.NewPoint(big.NewInt(7), big.NewInt(6))},
+		{p: ec2_2_17.NewPoint(big.NewInt(0), big.NewInt(6)), k: -1, expected: ec2_2_17.NewPoint(big.NewInt(0), big.NewInt(11))},
+	}
+
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("%d * %+v", tc.k, tc.p), func(t *testing.T) {
+			got := tc.p.ScalarMul(tc.k)
+
+			if !got.Eq(tc.expected) {
+				t.Errorf("got %+v, expected %+v", got, tc.expected)
+			}
+
+			if !tc.p.ec.IsOnCurve(got) {
+				t.Errorf("the result (%+v) is not on curve", got)
+			}
+		})
+	}
+}
